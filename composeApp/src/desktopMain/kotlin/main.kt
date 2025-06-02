5e8969abf26c4b26c9304adb9f9f01248aaeb70b
ori <orispok@gmail.com>
2025-06-02 19:29:13 +0300

    Add application window icon and set background color; enhance UI appearance

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,14 +1,22 @@
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.osg.openanimation.core.ui.util.icons.brandingpack.LogoVector
 import com.osg.openanimation.getBaseApp
 
 
 fun main() = application {
     val basePreviewApp = getBaseApp()
     Window(
+        icon = rememberVectorPainter(image = LogoVector),
         onCloseRequest = ::exitApplication,
         title = "OpenAnimation",
     ) {
+
+        window.background = java.awt.Color(Color.Green.toArgb())
+
         basePreviewApp.AppEntry()
     }
 }
